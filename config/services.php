d2dfe51c3633172fb50f5fc996c152cb78ba8d63
Abdur Rahman IsHa <abdurrahmanisha1@gmail.com>
2025-02-01 02:51:47 +0600

    1

--- config/services.php
+++ config/services.php
@@ -35,18 +35,18 @@ return [
         'client_id' => '480609137169-87nlnnblbu114342s78dokv84reg5cdp.apps.googleusercontent.com',
         'client_secret' => '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',
         'redirect' => 'http://localhost:8000/authorized/google/callback',
-],
+    ],
 
-'facebook' => [
-    'client_id' => 'mamurjorid',
-    'client_secret' => 'mamurjorsecret',
-    'redirect' => 'http://localhost:8000/callback',
-],
+    'facebook' => [
+        'client_id' => 'mamurjorid',
+        'client_secret' => 'mamurjorsecret',
+        'redirect' => 'http://localhost:8000/callback',
+    ],
 
-'github' => [
-    'client_id' => env('GITHUB_CLIENT_ID'),
-    'client_secret' => env('GITHUB_CLIENT_SECRET'),
-    'redirect' => 'http://localhost:8000/auth/github/callback',
-],
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => 'http://localhost:8000/auth/github/callback',
+    ],
 
 ];
